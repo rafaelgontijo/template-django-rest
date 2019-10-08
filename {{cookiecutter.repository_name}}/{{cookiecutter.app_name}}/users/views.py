953993ac7bc141b45c11a561671a8d8706e0fa2f
Rafael Gontijo Winter <rafaelgontijowinter@gmail.com>
2019-10-08 15:52:30 -0300

    Update views.py

--- {{cookiecutter.repository_name}}/{{cookiecutter.app_name}}/users/views.py
+++ {{cookiecutter.repository_name}}/{{cookiecutter.app_name}}/users/views.py
@@ -1,3 +1,4 @@
+from django_filters import rest_framework as filters
 from rest_framework import viewsets, mixins
 from rest_framework.permissions import AllowAny
 from .models import User
@@ -7,13 +8,20 @@ from .serializers import CreateUserSerializer, UserSerializer
 
 class UserViewSet(mixins.RetrieveModelMixin,
                   mixins.UpdateModelMixin,
+                  mixins.ListModelMixin,
                   viewsets.GenericViewSet):
     """
-    Updates and retrieves user accounts
+    List, updates and retrieves user accounts
     """
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = (IsUserOrReadOnly,)
+    permission_classes = (IsAdminUser, IsUserOrReadOnly,)
+    filter_backends = (filters.DjangoFilterBackend,)
+    filterset_fields = {
+        'name': ('icontains',),
+        'email': ('icontains',),
+    }
+
 
 
 class UserCreateViewSet(mixins.CreateModelMixin,
